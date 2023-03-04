7c400e7b11a9544f46ea5192cc9a24f1c10667c1
Thanh Dat <99642423+SturdyStudent@users.noreply.github.com>
2023-03-05 00:02:47 +0700

    Update rootRoute.js

--- src/routes/rootRoute.js
+++ src/routes/rootRoute.js
@@ -3,7 +3,9 @@ const rootRoute = express.Router();
 
 const orderRoute = require('./orderRoute');
 const likeResRoute = require('./likeResRoute');
+const rateResRoute = require( './rateResRoute' );
 
 rootRoute.use('/likeRes', likeResRoute);
-rootRoute.use('/user', orderRoute);
+rootRoute.use('/order', orderRoute);
+rootRoute.use('/rateRes', rateResRoute);
 module.exports = rootRoute;
